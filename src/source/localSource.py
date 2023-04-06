280e536b5614b8f6c9a10379563f9b8ef34aa378
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-04-06 12:53:18 -0300

    feat: Translate LocalSource model to python

---
+++ src/source/localSource.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from pathlib import Path
+from source import Source, SourceEntry
+
+@dataclass
+class FileEntry(SourceEntry):
+    name: str
+    path: Path
+
+class LocalSource(Source):
+    def readDir(self) -> list[FileEntry]:
+        dir: list[FileEntry] = []
+        for file in Path(self.config).iterdir():
+            if file.is_file():
+                dir.append(
+                    FileEntry(
+                        name=file.name,
+                        path=file.absolute()
+                    )
+                )
+        return dir
+    
+    def readFile(self, entry: FileEntry) -> str:
+        return entry.path.read_text()
+
