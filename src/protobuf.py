8fef02e9028118d2acf78f8c37d6d2196b53bae4
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-04-06 17:26:27 -0300

    refactor(protobuf): Move ternary to a function

--- src/protobuf.py
+++ src/protobuf.py
@@ -1,4 +1,5 @@
 from generator import Generator
+from parser import Entry
 
 class Protobuf(Generator):
     # kotlin type mapping
@@ -12,6 +13,7 @@ class Protobuf(Generator):
         # this should be auto discovered
         "UpdateStrategy": "int32",
     }
+    """Kotlin -> Protobuf type mapping"""
 
     def build(self) -> str:
         lines = ['syntax = "proto2";', '']
@@ -19,11 +21,19 @@ class Protobuf(Generator):
             lines.append(f"message {name} {{")
             
             for entry in self.defs[name]:
+                fieldType = self.get_type(entry)
+                attribute = self.get_attribute(entry)
                 lines.extend([
                     f"  // {entry.match.strip()}",
-                    f"  {'repeated' if entry.repeated else 'required' if entry.required else 'optional'} {self.get_type(entry)} {entry.name} = {entry.number};"
+                    f"  {attribute} {fieldType} {entry.name} = {entry.number};"
                 ])
             
-            lines.extend(["", "}"])  
+            lines.extend(["}", ""])  
         return '\n'.join(lines)
 
+    def get_attribute(self, entry: Entry) -> str:
+        if entry.repeated:
+            return "repeated"
+        if entry.required:
+            return "required"
+        return "optional"
