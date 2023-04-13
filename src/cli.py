a40a7a1c91f8600568c7ffa4d3be6c55819924fb
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-04-12 22:25:37 -0300

    feat(cli): Add a forgotten option and put options in groups

--- src/cli.py
+++ src/cli.py
@@ -6,13 +6,20 @@ from source.githubSource import GithubSource
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("-b", "--branch", help="Github repo branch", action="store", default="master")
-    args.add_argument("-r", "--repo", help="Github repo (user/repo_name)", action="store", default="tachiyomiorg/tachiyomi")
-    args.add_argument("-d", "--dir", help="models dir", action="store")
-    args.add_argument("-o", "--output", help="output file", action="store")
+
+    remote = args.add_argument_group("Github")
+    remote.add_argument("-b", "--branch", help="Remote repo branch (default: master)", action="store", default="master")
+    remote.add_argument("-r", "--repo", help="Remote repo address (user/repo_name) (default: tachiyomiorg/tachiyomi)", action="store", default="tachiyomiorg/tachiyomi")
+
+    local = args.add_argument_group("Local")
+    local.add_argument("-d", "--dir", help="Path to tachiyomi backup models (priority over github-related options)", action="store")
+
+    general = args.add_argument_group("General")
+    general.add_argument("-o", "--output", help="Output file (Stdout if not provided)", action="store")
+    general.add_argument("-i", "--add-invalid", help="Adds invalid types(BrokenBackup...), for backups before v0.12.2, but breaks protoc compatibility.", action="store_true")
     parsed = args.parse_args()
 
     source: Source = LocalSource(parsed.dir) if parsed.dir else GithubSource(parsed.repo, parsed.branch)
-    parser = Protobuf(source, True)
+    parser = Protobuf(source, parsed.add_invalid)
     parser.loadDefinition()
     parser.process(parsed.output)
